ce4aaa6e35c083daa027d70f06862d258a5f7a1a
ismail harmanda <iharmanda@hotmail.com>
2021-08-14 20:07:58 +0300

    error message implemented

--- src/App.js
+++ src/App.js
@@ -6,20 +6,28 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
   async function fetchMoviesHandler() {
-    setIsLoading(true);
-    const response = await fetch("https://swapi.dev/api/films");
-    const data = await response.json();
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
-    setIsLoading(false);
+    try {
+      setIsLoading(true);
+      const response = await fetch("https://swapi.dev/api/filmss");
+      if (!response.ok) {
+        throw new Error("Olağan üstü başarısızlıklar söz konusu.");
+      }
+      const data = await response.json();
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+      setIsLoading(false);
+    } catch (err) {
+      setError(err.message);
+    }
   }
   return (
     <React.Fragment>
@@ -31,7 +39,8 @@ function App() {
         {!isLoading && movies.length === 0 && (
           <p>There is no movies to show.</p>
         )}
-        {isLoading && <p>Loading...</p>}
+        {error === null && isLoading && <p>Loading...</p>}
+        {error !== null && <p>{error}</p>}
       </section>
     </React.Fragment>
   );
