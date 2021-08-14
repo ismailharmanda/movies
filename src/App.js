0b1599ba242d36146d8dcd6f8fb97614e4ce59db
ismail harmanda <iharmanda@hotmail.com>
2021-08-14 23:34:44 +0300

    post request and new get requests in App.js rebuilded.

--- src/App.js
+++ src/App.js
@@ -1,52 +1,86 @@
 import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
 import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+
   const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
     try {
-      setIsLoading(true);
       const response = await fetch(
         "https://movie-http-b527b-default-rtdb.europe-west1.firebasedatabase.app/movies.json"
       );
       if (!response.ok) {
-        throw new Error("Olağan üstü başarısızlıklar söz konusu.");
+        throw new Error("Something went wrong!");
       }
+
       const data = await response.json();
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
-      setIsLoading(false);
-    } catch (err) {
-      setError(err.message);
+
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+      setMovies(loadedMovies);
+    } catch (error) {
+      setError(error.message);
     }
+    setIsLoading(false);
   }, []);
+
   useEffect(() => {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
+
+  const addMovieHandler = async (movie) => {
+    const response = await fetch(
+      "https://movie-http-b527b-default-rtdb.europe-west1.firebasedatabase.app/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+  };
+
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && (
-          <p>There is no movies to show.</p>
-        )}
-        {error === null && isLoading && <p>Loading...</p>}
-        {error !== null && <p>{error}</p>}
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import Movie from './Movie';
-import classes from './MoviesList.module.css';
+import Movie from "./Movie";
+import classes from "./MoviesList.module.css";
 
 const MovieList = (props) => {
   return (
-    <ul className={classes['movies-list']}>
+    <ul className={classes["movies-list"]}>
       {props.movies.map((movie) => (
         <Movie
           key={movie.id}
           title={movie.title}
-          releaseDate={movie.release}
+          releaseDate={movie.releaseDate}
           openingText={movie.openingText}
         />
       ))}
