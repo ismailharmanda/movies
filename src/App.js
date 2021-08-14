20ed83cea66921979f31101ee5a61b5f6413d747
ismail harmanda <iharmanda@hotmail.com>
2021-08-14 19:32:39 +0300

    implemented loading screen

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     const response = await fetch("https://swapi.dev/api/films");
     const data = await response.json();
     const transformedMovies = data.results.map((movieData) => {
@@ -17,6 +19,7 @@ function App() {
       };
     });
     setMovies(transformedMovies);
+    setIsLoading(false);
   }
   return (
     <React.Fragment>
@@ -24,7 +27,8 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && <MoviesList movies={movies} />}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
