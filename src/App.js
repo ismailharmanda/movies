48c9798de83aa23e6090261715fd9b67e1d6d2eb
ismail harmanda <iharmanda@hotmail.com>
2021-08-14 19:34:13 +0300

    typo

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && movies.length && <MoviesList movies={movies} />}
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
         {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
